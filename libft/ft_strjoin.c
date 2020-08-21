423f5d9fa156f52054b5627a724eea0ecace0f1b
Hyechan Shin <kirikeria@gmail.com>
2020-08-21 18:50:31 +0300

    add heade

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyshin <kirikeria@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/29 11:58:05 by hyshin            #+#    #+#             */
-/*   Updated: 2020/08/21 18:45:42 by hyshin           ###   ########.fr       */
+/*   Created: 2020/08/21 18:49:40 by hyshin            #+#    #+#             */
+/*   Updated: 2020/08/21 18:49:42 by hyshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 
 	if (!s1 || !s2)
-	{
 		return (NULL);
-	}
 	res = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
 	if (res == NULL)
-	{
 		return (NULL);
-	}
 	ft_strcpy(res, s1);
 	ft_strcat(res, s2);
 	return (res);
